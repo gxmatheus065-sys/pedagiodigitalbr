4aaf124683ae11037cf497c3acfb597ed5aa8d98
gxmatheus065-sys <gxmatheus065@gmail.com>
2026-08-31 12:26:09 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,31 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  // 🔒 BLOQUEIO DE ACESSO DIRETO
+  async redirects() {
+    return [
+      {
+        // Se alguém tentar acessar a pasta física /admin diretamente...
+        source: '/admin/:path*',
+        // ...será barrado e jogado para a página inicial (Home)
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+
+  // 🎭 MÁSCARA DA URL SECRETA
+  async rewrites() {
+    return [
+      {
+        // Tudo o que você acessar usando /matrix-entry-adm/...
+        source: '/matrix-entry-adm/:path*',
+        // ...o Next.js vai ler por baixo dos panos na pasta física /admin/...
+        destination: '/admin/:path*',
+      },
+    ];
+  },
 }
 
 export default nextConfig
